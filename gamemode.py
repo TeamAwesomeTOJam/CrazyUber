c478045f0aae95547c64ce3a2366a0b982bd661f
Jonathan Doda <jonathan@jdoda.ca>
2016-05-07 19:00:25 -0400

    Put buildings in places. It's still very terrible at the moment.

--- gamemode.py
+++ gamemode.py
@@ -1,34 +1,46 @@
+import random
 import awesomeengine
+
 
 class GameMode(awesomeengine.mode.Mode):
 
     def enter(self):
         e = awesomeengine.get_engine()
 
-        for i in range(-50, 50):
-            for j in range(-50, 50):
-                e.add_entity('grass', x=i*20, y=j*20)
+        for i in range(-100, 100):
+            for j in range(-100, 100):
+                if i%8 == 0 or j%8 == 0:
+                    e.add_entity('road', x=i*20, y=j*20)
+                else:
+                    e.add_entity('grass', x=i*20, y=j*20)
 
-        building = e.add_entity('building')
+        e.entity_manager.commit_changes()        
+        
+        grass = list(e.entity_manager.get_by_tag('grass'))
+        random.shuffle(grass)
+        for tile in grass[:1000]:
+            print tile
+            e.add_entity('building', x=tile.x, y=tile.y)
 
         player = e.add_entity('player')
         player_cam_entity = e.add_entity('player-cam', target=player)
 
 
-        self.entities = [player_cam_entity, player, building]
+        self.entities = [player_cam_entity, player]
 
         for x in range(50):
             self.entities.append(e.add_entity('civilian-car', x = 20*x, y = 20, follow=player))
 
+        e.entity_manager.commit_changes()        
+        
         cam = e.create_camera(player_cam_entity,
                               layers=[awesomeengine.layer.SolidBackgroundLayer((100, 100, 100, 255)),
                                       awesomeengine.layer.SimpleCroppedLayer('terrain'),
-                                      awesomeengine.layer.GridLayer((200,200,200,255), 100),
-                                      # awesomeengine.layer.PhysicsLayer(),
+                                      awesomeengine.layer.PhysicsLayer(),
                                       awesomeengine.layer.SimpleCroppedLayer('draw')],
                               hud=[])
-
         self.cams = [cam]
+        
 
     def leave(self):
         e = awesomeengine.get_engine()

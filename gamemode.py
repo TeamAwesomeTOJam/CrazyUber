7fae944309a8f8caf0dfc52facbf7fc41473cde1
Jonathan Doda <jonathan@jdoda.ca>
2016-05-07 19:22:26 -0400

    Trying to place cars better. Not really working.

--- gamemode.py
+++ gamemode.py
@@ -9,17 +9,16 @@ class GameMode(awesomeengine.mode.Mode):
 
         for i in range(-100, 100):
             for j in range(-100, 100):
-                if i%8 == 0 or j%8 == 0:
+                if i%8 == 0 or (i-1) %8 == 0 or j%8 == 0 or (j-1)%8 == 0:
                     e.add_entity('road', x=i*20, y=j*20)
                 else:
                     e.add_entity('grass', x=i*20, y=j*20)
 
-        e.entity_manager.commit_changes()        
+        e.entity_manager.commit_changes()    
         
         grass = list(e.entity_manager.get_by_tag('grass'))
         random.shuffle(grass)
-        for tile in grass[:1000]:
-            print tile
+        for tile in grass[:10000]:
             e.add_entity('building', x=tile.x, y=tile.y)
 
         player = e.add_entity('player')
@@ -28,15 +27,18 @@ class GameMode(awesomeengine.mode.Mode):
 
         self.entities = [player_cam_entity, player]
 
-        for x in range(50):
-            self.entities.append(e.add_entity('civilian-car', x = 20*x, y = 20, follow=player))
+        road = list(e.entity_manager.get_by_tag('road'))
+        print len(road)
+        random.shuffle(road)
+        for tile in road[:50]:
+            self.entities.append(e.add_entity('civilian-car', x=tile.x, y=tile.y, follow=player))
 
         e.entity_manager.commit_changes()        
         
         cam = e.create_camera(player_cam_entity,
                               layers=[awesomeengine.layer.SolidBackgroundLayer((100, 100, 100, 255)),
                                       awesomeengine.layer.SimpleCroppedLayer('terrain'),
-                                      awesomeengine.layer.PhysicsLayer(),
+                                      #awesomeengine.layer.PhysicsLayer(),
                                       awesomeengine.layer.SimpleCroppedLayer('draw')],
                               hud=[])
         self.cams = [cam]

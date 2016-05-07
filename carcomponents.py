b3ce6591886eb700cde45b2738e0187024c7bd94
Daniel Lister <dan.lister@gmail.com>
2016-05-07 15:38:40 -0400

    push a car around

--- carcomponents.py
+++ carcomponents.py
@@ -2,6 +2,7 @@ from awesomeengine.component import verify_attrs
 from awesomeengine.component import Component
 from awesomeengine import engine
 from awesomeengine.vec2d import Vec2d
+from awesomeengine import rectangle
 
 import box2dcar
 
@@ -29,10 +30,11 @@ class InputCarComponent(Component):
 class Box2dCarComponent(Component):
 
     def add(self, entity):
-        verify_attrs(entity, [('max_lateral_impulse', 3), 'max_steering_angle', 'engine_force', ('steering_angle', 0), 'max_forward_speed', 'max_backward_speed', ('desired_speed', 0)] )
+        verify_attrs(entity, ['x', 'y', ('max_lateral_impulse', 3), 'max_steering_angle', 'engine_force', ('steering_angle', 0), 'max_forward_speed', 'max_backward_speed', ('desired_speed', 0)] )
         entity.box2d_car = box2dcar.TDCar(engine.get_engine().box2d_world,
                                           max_drive_force = entity.engine_force,
-                                          max_lateral_impulse= entity.max_lateral_impulse)
+                                          max_lateral_impulse= entity.max_lateral_impulse,
+                                          position=(entity.x, entity.y))
 
         entity.register_handler('update', self.handle_update)
 
@@ -45,6 +47,28 @@ class Box2dCarComponent(Component):
 
         entity.x = entity.box2d_car.body.position.x
         entity.y = entity.box2d_car.body.position.y
+
+        entity.x, entity.y = entity.box2d_car.body.GetWorldPoint((0,0))
         entity.angle = math.degrees(entity.box2d_car.body.angle) + 90
+
+        engine.get_engine().entity_manager.update_position(entity)
+
+class CarDrawComponent(Component):
+
+    def add(self, entity):
+        verify_attrs(entity, ['width', 'height'])
+
+        entity.register_handler('draw', self.handle_draw)
+
+    def remove(self, entity):
+        entity.unregister_handler('draw', self.handle_draw)
+
+    def handle_draw(self, entity, camera):
+        x,y = entity.box2d_car.body.GetWorldPoint((0, 0))
+        angle = math.degrees(entity.box2d_car.body.angle) + 90
+
+        rect = rectangle.Rect(x,y,entity.width, entity.height, angle)
+
+        camera.draw_rect((0,255,0,255), rect)
 
 
--- gamemode.py
+++ gamemode.py
@@ -10,7 +10,9 @@ class GameMode(awesomeengine.mode.Mode):
         player = e.add_entity('player')
         player_cam_entity = e.add_entity('player-cam', target=player)
 
-        self.entities = [player_cam_entity, player, building]
+        test_car = e.add_entity('car', x = 0, y = 100)
+
+        self.entities = [player_cam_entity, player, building, test_car]
 
         cam = e.create_camera(player_cam_entity,
                               layers=[awesomeengine.layer.SolidBackgroundLayer((100, 100, 100, 255)),

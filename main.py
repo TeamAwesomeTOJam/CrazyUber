63dde92083f34f1587c13e6c645937f58156ae72
Daniel Lister <dan.lister@gmail.com>
2016-05-11 15:14:28 -0400

    updateo for screen resolution detection

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def go():
     engine.add_mode('attract', attractmode.AttractMode())
     engine.add_mode('game', gamemode.GameMode())
 
-    engine.create_window((2560, 1440), title='CrazyUber')
+    engine.create_window(title='CrazyUber')
     engine.change_mode('attract')
 
     engine.window.set_fullscreen(True)
